1eaf0e09a0cf7f60ad533dd95ab975b729a524d3
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2021-09-18 19:11:48 -0300

    Creation of repository

---
+++ NLW Together - ReactJS/letmeask/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <>
+      <Button text="Botão 1"/>
+      <Button text="Botão 2"/>
+      <Button text="Botão 3"/>
+    </>
+  );
+}
+
+export default App;
---
+++ NLW Together - ReactJS/letmeask/src/components/Button.tsx
@@ -0,0 +1,11 @@
+type ButtonProps = {
+    text: string,
+}
+
+export const Button = (props: ButtonProps) => {
+    return (
+        <button>
+            { props.text}
+        </button>
+    )
+}
